d7751383b4404b12702997d9a984b1830af49676
Akimoto <jaxsonwang@foxmail.com>
2020-07-28 09:44:59 +0800

    更新 PlayerController

--- MyMMO/Assets/_Scripts/PlayerController.cs
+++ MyMMO/Assets/_Scripts/PlayerController.cs
@@ -23,6 +23,28 @@ public class PlayerController : MonoBehaviour
     private const float groundedDistance = 0.5f;
     private bool grounded;
     private bool walking;
+
+    float SidestepAxisInput
+    {
+        get
+            {
+            if (Input.GetMouseButtonDown(1))
+            {
+                float sidestep = Input.GetAxis("Sidestep");
+                float horizontal = Input.GetAxis("Horizontal");
+                return Mathf.Abs(sidestep) > Mathf.Abs(horizontal) ? sidestep : horizontal;
+            }
+            else
+            {
+                return Input.GetAxis("Sidestep");
+            }
+        }
+    }
+
+    public bool Grounded
+    {
+        get { return grounded; }
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -45,7 +67,7 @@ public class PlayerController : MonoBehaviour
     {
         float rotationAmount;
         if (Input.GetMouseButton(1) && (!requireLock || controlLock || Cursor.lockState == CursorLockMode.Locked))
-        {
+        {         
             if (controlLock)
             {
                 Cursor.lockState = CursorLockMode.Locked;
@@ -56,12 +78,83 @@ public class PlayerController : MonoBehaviour
                 if (controlLock)
                 {
                     Cursor.lockState = CursorLockMode.None;
-                    rotationAmount = Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime;
+                    
                 }
+                rotationAmount = Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime;
             }
             target.transform.RotateAround(target.transform.up, rotationAmount);
-            
+            if (Input.GetButtonDown("ToggleWalk")) {
+                walking = !walking;
+            }
         
         }
     }
+
+    void FixedUpdate()
+    // Handle movement here since physics will only be calculated in fixed frames anyway
+    {
+        grounded = Physics.Raycast(
+            target.transform.position + target.transform.up * -groundedCheckOffset,
+            target.transform.up * -1,
+            groundedDistance,
+            groundLayers
+        );
+        // Shoot a ray downward to see if we're touching the ground
+
+        if (grounded)
+        {
+            target.drag = groundDrag;
+            // Apply drag when we're grounded
+
+            if (Input.GetButton("Jump"))
+            // Handle jumping
+            {
+                target.AddForce(
+                    jumpSpeed * target.transform.up +
+                        target.velocity.normalized * directionalJumpFactor,
+                    ForceMode.VelocityChange
+                );
+                // When jumping, we set the velocity upward with our jump speed
+                // plus some application of directional movement
+
+                if (onJump != null)
+                {
+                    onJump();
+                }
+            }
+            else
+            // Only allow movement controls if we did not just jump
+            {
+                Vector3 movement = Input.GetAxis("Vertical") * target.transform.forward +
+                    SidestepAxisInput * target.transform.right;
+
+                float appliedSpeed = walking ? speed / walkSpeedDownscale : speed;
+                // Scale down applied speed if in walk mode
+
+                if (Input.GetAxis("Vertical") < 0.0f)
+                // Scale down applied speed if walking backwards
+                {
+                    appliedSpeed /= walkSpeedDownscale;
+                }
+
+                if (movement.magnitude > inputThreshold)
+                // Only apply movement if we have sufficient input
+                {
+                    target.AddForce(movement.normalized * appliedSpeed, ForceMode.VelocityChange);
+                }
+                else
+                // If we are grounded and don't have significant input, just stop horizontal movement
+                {
+                    target.velocity = new Vector3(0.0f, target.velocity.y, 0.0f);
+                    return;
+                }
+            }
+        }
+        else
+        {
+            target.drag = 0.0f;
+            // If we're airborne, we should have no drag
+        }
+    }
+
 }
